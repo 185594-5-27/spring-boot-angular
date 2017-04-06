29f54ce9dedde414b1d185c195da69bc96fff1f9
林泽锋 <282245889@qq.com>
2017-04-06 17:19:00 +0800

    Initial commit

---
+++ src/main/java/com/spring/SpringBootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApplication.class, args);
+	}
+}
